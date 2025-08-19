eb0fcd043d617b6c77fce0cd756acc446b9783eb
Lindemberg da Silva Justino <lindembergparaiba@gmail.com>
2025-08-19 09:20:17 -0300

    feat: desafio concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // DONE: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count())
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // DONE: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new ArgumentException(message: "Capacidade menor que a quantidade de hospedes recebido!");
             }
         }
 
@@ -37,7 +38,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
@@ -45,16 +46,16 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valorDiaria = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valorDiaria = valorDiaria * 0.9m;
             }
 
-            return valor;
+            return valorDiaria;
         }
     }
 }
